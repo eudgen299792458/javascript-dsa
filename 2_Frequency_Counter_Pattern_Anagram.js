72db73d4a22ca2963cd76b27df424d4000f25af1
genGit963 <bogati.mahesh.299.792.458@gmail.com>
2024-11-21 11:06:35 +0545

    🔖: Frequency Counter Pattern Anagaram: Solution, Method 2

--- 2_Frequency_Counter_Pattern_Anagram.js
+++ 2_Frequency_Counter_Pattern_Anagram.js
@@ -1,6 +1,9 @@
 // directly implementing the frequency Counter Pattern method
-
+// method 1: O(5n) -> O(n)
 function checkAnagrams(string1, string2) {
+  string1 = string1.replaceAll(/\s/g, "").toLowerCase();
+  string2 = string2.replaceAll(/\s/g, "").toLowerCase();
+  if (string1.length != string2.length) return false;
   let freqCounterString1 = {};
   let freqCounterString2 = {};
 
@@ -13,7 +16,7 @@ function checkAnagrams(string1, string2) {
   }
 
   //   evaluation
-  console.log(freqCounterString1, "\n", freqCounterString2);
+
   for (let key in freqCounterString1) {
     if (
       !(
@@ -26,12 +29,12 @@ function checkAnagrams(string1, string2) {
   return true;
 }
 
-console.log("\n---------- TEST CASE ----------");
+console.log("\n---------- 1. method, TEST CASES: 2 ----------");
 // Test Case 1
 const s1 = "listen";
 const s2 = "silent";
 console.log(
-  `Test Case 1: ${s1} and ${s2} are anagrams?: `,
+  `Test Case 1: '${s1}' and '${s2}' are anagrams?: `,
   checkAnagrams(s1, s2)
 ); // should return true
 
@@ -39,7 +42,7 @@ console.log(
 const s1_2 = "triangle";
 const s2_2 = "integral";
 console.log(
-  `Test Case 2: ${s1_2} and ${s2_2} are anagrams?: `,
+  `Test Case 2: '${s1_2}' and '${s2_2}' are anagrams?: `,
   checkAnagrams(s1_2, s2_2)
 ); // should return true
 
@@ -47,14 +50,71 @@ console.log(
 const s1_3 = "apple";
 const s2_3 = "pale";
 console.log(
-  `Test Case 3: ${s1_3} and ${s2_3} are anagrams?: `,
+  `Test Case 3: '${s1_3}' and '${s2_3}' are anagrams?: `,
   checkAnagrams(s1_3, s2_3)
 ); // should return false
 
 // Test Case 4
-const s1_4 = "I am Lord Voldemort";
-const s2_4 = "Tom Marvolo Riddle";
+const s1_4 = "IamLordVoldemort";
+const s2_4 = "TomMarvoloRiddle";
 console.log(
-  `Test Case 4: ${s1_4} and ${s2_4} are anagrams?: `,
+  `Test Case 4: '${s1_4}' and '${s2_4}' are anagrams?: `,
   checkAnagrams(s1_4, s2_4)
+); // should return true
+
+// Test Case 5
+const s1_5 = "I am Lord Voldemort";
+const s2_5 = "Tom Marvolo Riddle";
+console.log(
+  `Test Case 5: '${s1_5}' and '${s2_5}' are anagrams?: `,
+  checkAnagrams(s1_5, s2_5)
+); // should return true
+
+// method 2: o(2n)->O(n)
+function isAnagrams(string1, string2) {
+  if (string1.length != string2.length) return false;
+
+  let freqCounterString1 = {};
+  for (let value of string1) {
+    freqCounterString1[value] = (freqCounterString1[value] || 0) + 1;
+  }
+
+  for (let value of string2) {
+    if (!value in freqCounterString1) {
+      return false;
+    } else {
+      freqCounterString1[value] -= 1;
+    }
+  }
+  return true;
+}
+
+console.log("\n---------- 2. method, TEST CASES: 2 ----------");
+// Test Case 1
+console.log(
+  `Test Case 1: '${s1}' and '${s2}' are anagrams?: `,
+  isAnagrams(s1, s2)
+); // should return true
+
+// Test Case 2
+console.log(
+  `Test Case 2: '${s1_2}' and '${s2_2}' are anagrams?: `,
+  isAnagrams(s1_2, s2_2)
+); // should return true
+
+// Test Case 3
+console.log(
+  `Test Case 3: '${s1_3}' and '${s2_3}' are anagrams?: `,
+  isAnagrams(s1_3, s2_3)
+); // should return false
+
+// Test Case 4
+console.log(
+  `Test Case 4: '${s1_4}' and '${s2_4}' are anagrams?: `,
+  isAnagrams(s1_4, s2_4)
+); // should return true
+
+console.log(
+  `Test Case 5: '${s1_5}' and '${s2_5}' are anagrams?: `,
+  isAnagrams(s1_5, s2_5)
 ); // should return true
