ea142e0ae9fbe6ab79dee67867a2f84d2f92ac32
genGit963 <bogati.mahesh.299.792.458@gmail.com>
2024-11-23 11:39:40 +0545

    🔃: Re-cursion: normal factorial function

---
+++ 2. Recursion/1_factorial.js
@@ -0,0 +1,11 @@
+function factorial(n) {
+  if (n == 1) {
+    return 1;
+  }
+  console.log(`${n} * factorial(${n - 1})`);
+  return n * factorial(n - 1);
+}
+
+const value = factorial(5);
+
+console.log("final:", value);
