3c909385f8c9fa7c5197e7fb0d2478362126cdad
genGit963 <bogati.mahesh.299.792.458@gmail.com>
2024-12-13 20:50:32 +0545

    Singly-linked-list OPERATIONS

--- 6. Singly-linked-list/main.js
+++ 6. Singly-linked-list/main.js
@@ -29,6 +29,7 @@ class SinglyLinkedList {
     var current = this.head;
     let head = this.head;
     while (current) {
+      console.log("Node :- ", current);
       current = current.next;
     }
     return { head, tail: current };
@@ -74,7 +75,7 @@ class SinglyLinkedList {
     }
 
     let currentNode = this.head;
-    let newHead = this.head;
+    const newHead = this.head;
 
     while (currentNode.next) {
       newHead = currentNode;
@@ -103,13 +104,72 @@ class SinglyLinkedList {
    * @description this return given nth place node value
    * @param {number} n number
    */
-  get(n) {
+  get(indexOf) {
+    const check = this.validateIndex(indexOf);
+    if (check) {
+      return check;
+    }
     let requiredNode = this.head;
-    if (n > this.length) return null;
-    for (let index = 0; index < n; index++) {
+    for (let index = 0; index < indexOf; index++) {
       requiredNode = requiredNode.next;
     }
     return requiredNode;
+  }
+
+  set(value, index) {
+    const check = this.validateIndex(index);
+    if (check) {
+      return check;
+    }
+    let indexOfNode = this.get(index);
+    indexOfNode.value = value;
+    return this;
+  }
+
+  insert(value, index) {
+    const check = this.validateIndex(index);
+    if (check) {
+      return check;
+    }
+    const beforeIndexOfNode = this.get(index - 1);
+    const indexOfNode = this.get(index);
+
+    const newNode = new Node(value);
+    beforeIndexOfNode.next = newNode;
+    newNode.next = indexOfNode;
+    this.length++;
+
+    return this;
+  }
+
+  remove(index) {
+    const check = this.validateIndex(index);
+    if (check) {
+      return check;
+    }
+    let indexOfNode = this.get(index);
+    const beforeIndexOfNode = this.get(index - 1);
+    const afterIndexOfNode = this.get(index + 1);
+
+    beforeIndexOfNode.next = afterIndexOfNode;
+    indexOfNode = null;
+    this.length--;
+
+    return this;
+  }
+
+  reverse() {
+    const reverseList = new SinglyLinkedList();
+    reverseList.tail = this.head;
+    reverseList.head = this.tail;
+    reverseList.length = this.length;
+    return reverseList;
+  }
+
+  validateIndex(index) {
+    if (this.length == 0) return "Err: cannot set to empty list";
+    if (index > this.length) return "Err: index greater than list length";
+    if (index < 0) return "Err: provide index ≥ 0";
   }
 }
 
@@ -117,10 +177,17 @@ const list = new SinglyLinkedList();
 
 list.push("1");
 list.push("2");
+list.push("3");
 list.push("4");
+list.push("5");
 
-list.traverse();
 // console.log(list.pop());
 // console.log(list.shift());
 // console.log(list.unshift(6));
-console.log(list.get(1));
+// console.log(list.get(1));
+// console.log(list.set("AA", 2));
+// console.log(list.insert("A", 2));
+// console.log(list.remove(1));
+
+// list.traverse();
+console.log(list.reverse());
