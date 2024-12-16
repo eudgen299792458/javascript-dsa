7d44176f8800823e9679e9785ca9f16015750ccc
genGit963 <bogati.mahesh.299.792.458@gmail.com>
2024-12-16 17:32:42 +0545

    ✅: Doubly-linked-list-> pop(), shift()

--- 7. Doubly-linked-list/main.js
+++ 7. Doubly-linked-list/main.js
@@ -25,7 +25,22 @@ class DoublyLinkedList {
     this.tail.next = newNode;
     this.tail = newNode;
     newNode.prev = oldTail;
+    this.length++;
     return;
+  }
+
+  pop() {
+    const nodebeforeTail = this.tail.prev;
+    nodebeforeTail.next = null;
+    this.tail = nodebeforeTail;
+    this.length--;
+  }
+
+  shift() {
+    const nodeAfterHead = this.head.next;
+    nodeAfterHead.prev = null;
+    this.head = nodeAfterHead;
+    this.length--;
   }
 
   traverse(direction = "forward") {
@@ -61,5 +76,16 @@ dlist.push(4);
 dlist.push(5);
 dlist.push(6);
 dlist.push(7);
+
+// pop()
+dlist.pop();
+console.log("\nafter pop tail: ", dlist.tail);
+
+// shift()
+dlist.shift();
+console.log("\nafter shift head:", dlist.head);
+
+// display
 dlist.display();
 dlist.display("backward");
+console.log("length: ", dlist.length);
