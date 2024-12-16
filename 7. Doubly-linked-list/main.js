c966ec2922aacaa402d3fe78cbd983b7a66c2718
genGit963 <bogati.mahesh.299.792.458@gmail.com>
2024-12-16 16:48:00 +0545

    ✅: Doubly-linked-list-> push()

---
+++ 7. Doubly-linked-list/main.js
@@ -0,0 +1,65 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+      this.length++;
+      return;
+    }
+    const oldTail = this.tail;
+    this.tail.next = newNode;
+    this.tail = newNode;
+    newNode.prev = oldTail;
+    return;
+  }
+
+  traverse(direction = "forward") {
+    const directionStatus = direction === "forward" ? true : false;
+    let currentNode = directionStatus ? this.head : this.tail;
+
+    if (directionStatus) {
+      while (currentNode.next) {
+        console.log("Node: ", currentNode);
+        currentNode = currentNode.next;
+      }
+    } else {
+      while (currentNode.prev) {
+        console.log("Node: ", currentNode);
+        currentNode = currentNode.prev;
+      }
+    }
+
+    console.log("Node: ", currentNode);
+  }
+
+  display(direction = "forward") {
+    console.log("\n-----------------list: ", direction + "------------------");
+    this.traverse(direction);
+    console.log("----------------------eol------------------------");
+  }
+}
+
+const dlist = new DoublyLinkedList();
+
+dlist.push(3);
+dlist.push(4);
+dlist.push(5);
+dlist.push(6);
+dlist.push(7);
+dlist.display();
+dlist.display("backward");
