471cba4704a143ecc9fd6d58fe9a79190dbf08aa
genGit963 <bogati.mahesh.299.792.458@gmail.com>
2025-01-06 00:24:10 +0545

    🚀 HASHING: done

---
+++ 12. Hash/hash_function.js
@@ -0,0 +1,27 @@
+function hash(key, arrayLen) {
+  let total = 0;
+  const WEIRED_PRIME = 31; // to avoid collision btw multiple keys pointing to same value
+  for (let i = 0; i < Math.min(key.length, 100); i++) {
+    // max chars will be take upto first 100, if two keys have same first 100 chars, they will colide
+    const char = key[i];
+    const value = char.charCodeAt(0) - 96;
+    total = (total * WEIRED_PRIME + value) % arrayLen;
+  }
+
+  return total;
+}
+
+// test
+const HASH_TABLE_SIZE = 3;
+for (const key of [
+  "keya",
+  "keyb",
+  "keyc",
+  "keyd",
+  "keye",
+  "keyf",
+  "keyg",
+  "keyh",
+]) {
+  console.log("key, hash(key) : ", key, hash(key, HASH_TABLE_SIZE));
+}
